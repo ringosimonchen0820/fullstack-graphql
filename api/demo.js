090a6822bc8554a677a1ecf6158bc0fa718536a2
Simon Chen <simonchenshufu@Simons-MacBook-Pro.local>
2020-06-04 18:04:44 -0700

    basic graphql defs

--- api/demo.js
+++ api/demo.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
 // 1. Create Query Type in the Schema using SDL
-    // Add fields to the Query Type
+//      Add fields to the Query Type
 const typeDefs = gql `
     type User {
         email: String! # the ! means not nullable 
@@ -10,14 +10,29 @@ const typeDefs = gql `
         friends: [User!]! # this means that the friends must always be an array and also must have user inside 
     }
 
-    # query type is always necessary
-        # A type on a Schema that defines operations clients can perform to access data that resembles the shape of the other Types in the Schema.
+    # Query type:
+    #     A type on a Schema that defines operations clients can perform to 
+    #     access data that resembles the shape of the other Types 
+    #     in the Schema.
     type Query {
         me: User!
     }
 `
 
 // 2. Create Resolvers for the fields
+/*
+    # resolvers: 
+        Function that are responsible for returning values
+        for fields that exist on Types in a Schema.
+        Resolvers execution is dependent on the incoming
+        client Query
+    
+    # creating resolvers:
+        1.  Resolver names must match the exact field name on your Schema's Type
+        2.  Resolvers must return the value type declared for the matching field
+        3.  Resolvers can be async
+        4.  Can retrieve data from any source
+*/
 const resolvers = {
     Query: {
         me() {
